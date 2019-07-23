3a89b34accef44082115ef5931de8ef72923965d
Conor McCarthy <cmccarthy1@kx.com>
2019-07-23 12:37:47 +0100

    more legible modification method for windows build

--- build/build.bat
+++ build/build.bat
@@ -3,7 +3,7 @@ echo.
 echo This script is used to build the 64 bit interface between kafka and kdb+
 echo.
 echo Please ensure you have followed the instructions outlined in the README.md file on github.
-echo In particular add the modifications to the include ("/I")in this script must be completed as outlined
+echo In particular add the modifications to the include ("/I") within this script must be completed as outlined
 echo.
 :PROMPT
 SET /P AREYOUSURE=Are you happy this has all been completed (Y/[N])?
@@ -13,10 +13,12 @@ IF /I "%AREYOUSURE%" NEQ "Y" GOTO END
 curl -fsSL -o k.h          https://github.com/KxSystems/kdb/raw/master/c/c/k.h                   || goto :error
 curl -fsSL -o q.lib        https://github.com/KxSystems/kdb/raw/master/w64/q.lib                 || goto :error
 
+set include_path="C:/Users/jkandola/librdkafka.redist.1.0.0/build/native/include/"
+
 ::keep original PATH, PATH may get too long otherwise
 set OP=%PATH%
 call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvars64.bat"
-cl /LD /DKXVER=3 /I"C:/Users/jkandola/librdkafka.redist.1.0.0/build/native/include/" /Felibkfk.dll /O2 kfk.c q.lib
+cl /LD /DKXVER=3 /I%include_path% /Felibkfk.dll /O2 kfk.c q.lib
 set PATH=%OP%
 
 exit /b 0
