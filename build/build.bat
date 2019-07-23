93df74e779dc6fba3bfa14bc3024cbd1c9f8d49b
Conor McCarthy <cmccarthy1@kx.com>
2019-07-23 14:15:09 +0100

    Final present change to bat file

--- build/build.bat
+++ build/build.bat
@@ -1,9 +1,10 @@
 @echo off
 echo.
 echo This script is used to build the 64 bit interface between kafka and kdb+
-echo.
+cho.
 echo Please ensure you have followed the instructions outlined in the README.md file on github.
-echo In particular add the modifications to the include ("/I") within this script must be completed as outlined
+echo In particular add the modifications to user_path within this script if librdkafka.redist.1.0.0 is not in QHOME.
+echo Default expected location for librdkafka.redist.1.0.0 is QHOME
 echo.
 :PROMPT
 SET /P AREYOUSURE=Are you happy this has all been completed (Y/[N])?
@@ -13,7 +14,7 @@ IF /I "%AREYOUSURE%" NEQ "Y" GOTO END
 curl -fsSL -o k.h          https://github.com/KxSystems/kdb/raw/master/c/c/k.h                   || goto :error
 curl -fsSL -o q.lib        https://github.com/KxSystems/kdb/raw/master/w64/q.lib                 || goto :error
 
-set user_path="C:/Users/jkandola"
+set user_path="%QHOME%"
 set include_path="%user_path%/librdkafka.redist.1.0.0/build/native/include/"
 
 ::keep original PATH, PATH may get too long otherwise
