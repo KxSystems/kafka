8330a5b2404b47c1c4570b15b047771211a918dc
Conor McCarthy <cmccarthy1@kx.com>
2019-07-23 14:19:00 +0100

    revert of bat file

--- build/build.bat
+++ build/build.bat
@@ -3,8 +3,7 @@ echo.
 echo This script is used to build the 64 bit interface between kafka and kdb+
 cho.
 echo Please ensure you have followed the instructions outlined in the README.md file on github.
-echo In particular add the modifications to user_path within this script if librdkafka.redist.1.0.0 is not in QHOME.
-echo Default expected location for librdkafka.redist.1.0.0 is QHOME
+echo In particular add the modifications to user_path within this script to point to your librdkafka.redist.1.0.0 location.
 echo.
 :PROMPT
 SET /P AREYOUSURE=Are you happy this has all been completed (Y/[N])?
@@ -14,7 +13,7 @@ IF /I "%AREYOUSURE%" NEQ "Y" GOTO END
 curl -fsSL -o k.h          https://github.com/KxSystems/kdb/raw/master/c/c/k.h                   || goto :error
 curl -fsSL -o q.lib        https://github.com/KxSystems/kdb/raw/master/w64/q.lib                 || goto :error
 
-set user_path="%QHOME%"
+set user_path="C:/Users/jkandola"
 set include_path="%user_path%/librdkafka.redist.1.0.0/build/native/include/"
 
 ::keep original PATH, PATH may get too long otherwise
