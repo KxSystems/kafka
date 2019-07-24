35a208cd9600eef57afb2b73ac8d43b22a7c10dc
Conor McCarthy <cmccarthy1@kx.com>
2019-07-24 16:41:53 +0100

    Initial test of user input librdkafka.redist.1.0.0

--- build/build.bat
+++ build/build.bat
@@ -1,7 +1,10 @@
 @echo off
 echo.
 echo This script is used to build the 64 bit interface between kafka and kdb+
-cho.
+echo.
+
+set /p user_path = Please add in the path to location of `librdkafka.redist.1.0.0`:
+
 echo Please ensure you have followed the instructions outlined in the README.md file on github.
 echo In particular add the modifications to user_path within this script to point to your librdkafka.redist.1.0.0 location.
 echo.
@@ -13,8 +16,7 @@ IF /I "%AREYOUSURE%" NEQ "Y" GOTO END
 curl -fsSL -o k.h          https://github.com/KxSystems/kdb/raw/master/c/c/k.h                   || goto :error
 curl -fsSL -o q.lib        https://github.com/KxSystems/kdb/raw/master/w64/q.lib                 || goto :error
 
-set user_path="C:/Users/jkandola"
-set include_path="%user_path%/librdkafka.redist.1.0.0/build/native/include/"
+set include_path="%1/librdkafka.redist.1.0.0/build/native/include/"
 
 ::keep original PATH, PATH may get too long otherwise
 set OP=%PATH%
