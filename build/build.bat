f333883d763a45abdfd21eebcd1c93bd2153a3e7
cmccarthy1 <cmccarthy1@kx.com>
2019-08-07 19:13:03 +0100

    Modifications to paths to ensure correct build

--- build/build.bat
+++ build/build.bat
@@ -13,15 +13,15 @@ SET /P AREYOUSURE=Are you happy this has all been completed (Y/[N])?
 IF  /I "%AREYOUSURE%" NEQ "Y" GOTO END
 echo.
 :: Standalone build
-curl -fsSL -o k.h   https://github.com/KxSystems/kdb/raw/master/c/c/k.h      || goto :error
-curl -fsSL -o q.lib https://github.com/KxSystems/kdb/raw/master/w64/q.lib    || goto :error
+curl -fsSL -o ../k.h   https://github.com/KxSystems/kdb/raw/master/c/c/k.h      || goto :error
+curl -fsSL -o ../q.lib https://github.com/KxSystems/kdb/raw/master/w64/q.lib    || goto :error
 
 SET INCLUDE_PATH="%USER_PATH%/librdkafka.redist.1.0.0/build/native/include/"
 
 ::keep original PATH, PATH may get too long otherwise
 set OP=%PATH%
 call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvars64.bat"
-cl /LD /DKXVER=3 /I%INCLUDE_PATH% /Felibkfk.dll /O2 ../kfk.c q.lib
+cl /LD /DKXVER=3 /I%INCLUDE_PATH% /Felibkfk.dll /O2 ../kfk.c ../q.lib
 set PATH=%OP%
 
 exit /b 0
